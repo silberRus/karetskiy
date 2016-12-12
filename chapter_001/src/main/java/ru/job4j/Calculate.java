da643c7310f6f4e2adeb634a6b3ec1d3dc3e8730
silberRus <kareckiyan@gmail.com>
2016-12-12 22:30:43 +0300

    подготовил файл согласно задание 1.3

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,9 +1,18 @@
 package ru.job4j;
 
+/**
+* Class Класс калькулятор обучение, сейчас только hello world
+* @autor karetskiy
+* @since 12.12.2016
+* @version 1
+*/
 public class Calculate{
 
-	public static void main(String[] args)
-	{
-		System.out.println("Helo World!");
+	/**
+	* Точка входа программы, выводит hello world в консоль
+	* @param arg - arg
+	*/
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
 	}
 }
