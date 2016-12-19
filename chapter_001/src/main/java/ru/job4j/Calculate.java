ed1d3e8fdcf9dae548161a39c044218060a8a8d2
silberRus <kareckiyan@gmail.com>
2016-12-20 00:30:07 +0300

    написан класс калькулятор плс минус делить умножить

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,17 +1,63 @@
 package ru.job4j;
+
 /**
-* Class Класс калькулятор обучение, сейчас только hello world.
+* Class Класс калькулятор обучение, сложение, вычитание, умножение и деление.
 * @author karetskiy
-* @since 12.12.2016
-* @version 1
+* @since 19.12.2016
+* @version 2
 */
 public class Calculate {
 
 	/**
-	* Точка входа программы, выводит hello world в консоль.
-	* @param arg - arg.
+	* Память калькулятора.
 	*/
-	public static void main(String[] arg) {
-		System.out.println("Hello World");
+	private double result;
+
+	/**
+	* Возвращает результат из памяти калькулятора.
+	* @return result – реультат калькулятора:
+	*/
+	public double getResult() {
+		return this.result;
+	}
+
+	/**
+	* Вычисляет сумму двух чисел, и записывает результат
+	* во внутреннюю переменную класса result.
+	* @param first - первое число
+	* @param second - второе число
+	*/
+	public void add(double first, double second) {
+		this.result = first + second;
+	}
+
+	/**
+	* Удаляет из первого числа второе, и записывает результат
+	* во внутреннюю переменную класса result.
+	* @param first - первое число
+	* @param second - второе число
+	*/
+	public void substruct(double first, double second) {
+		this.result = first - second;
+	}
+
+	/**
+	* делит второе число на первое, и записывает результат
+	* во внутреннюю переменную класса result.
+	* @param first - первое число
+	* @param second - второе число
+	*/
+	public void div(double first, double second) {
+		this.result = first / second;
+	}
+
+	/**
+	* умножает числа, и записывает результат
+	* во внутреннюю переменную класса result.
+	* @param first - первое число
+	* @param second - второе число
+	*/
+	public void multiple(double first, double second) {
+		this.result = first * second;
 	}
 }
--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -1,8 +1,6 @@
 package ru.job4j;
 
 import org.junit.Test;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -10,27 +8,60 @@ import static org.junit.Assert.assertThat;
 * Test.
 *
 * @author Karetskiy
-* @version 1.0
-* @since 14.12.2016
+* @version 2.0
+* @since 19.12.2016
 */
-
 public class CalculateTest {
+
 	/**
-	* Test add.
+	* Test сложения.
 	*/
 	@Test
-	public void whenAddOneToOneThenTwo() {
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		System.setOut(new PrintStream(out));
-		Calculate.main(null);
-		assertThat(
-                out.toString(),
-                is(
-                        String.format(
-                                "Hello World%s",
-                                System.getProperty("line.separator")
-                        )
-                )
-        );
+	public void whenAddTwoToFourThenSix() {
+
+		final Calculate calc = new Calculate();
+		calc.add(2, 4);
+
+		final double result = calc.getResult();
+		assertThat(result, is(6d));
+	}
+
+	/**
+	* Test вычитания.
+	*/
+	@Test
+	public void whenSubstructSevenToTwoThenFive() {
+
+		final Calculate calc = new Calculate();
+		calc.substruct(7, 2);
+
+		final double result = calc.getResult();
+		assertThat(result, is(5d));
+	}
+
+	/**
+	* Test деления.
+	*/
+	@Test
+	public void whenDivFourToTwoThenTwo() {
+
+		final Calculate calc = new Calculate();
+		calc.div(4, 2);
+
+		final double result = calc.getResult();
+		assertThat(result, is(2d));
+	}
+
+	/**
+	* Test умножения.
+	*/
+	@Test
+	public void whenMultipleFourToTwoThenEight() {
+
+		final Calculate calc = new Calculate();
+		calc.multiple(4, 2);
+
+		final double result = calc.getResult();
+		assertThat(result, is(8d));
 	}
 }
