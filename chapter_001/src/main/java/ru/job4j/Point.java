17fa77f69c680cd70d28fe17ea527d5759e8ff9a
silberRus <kareckiyan@gmail.com>
2016-12-30 00:22:34 +0300

    Добавил модуль под корень

--- chapter_001/src/main/java/ru/job4j/Point.java
+++ chapter_001/src/main/java/ru/job4j/Point.java
@@ -33,7 +33,7 @@ public class Point {
 
 		//calculate distance between two points
 
-		return Math.sqrt((Math.pow(point.x, 2) - Math.pow(this.x, 2)) - (Math.pow(point.y, 2) - Math.pow(this.y, 2)));
+		return Math.sqrt(Math.abs(Math.pow(point.x - this.x, 2)) + Math.abs(Math.pow(point.y - this.y, 2)));
 	}
 }
 
