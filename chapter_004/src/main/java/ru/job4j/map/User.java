d6b1b8eae9ab3245d1baee664f06a628a40ecb65
Andrey Karetskiy <kareckiyan@gmail.com>
2018-05-16 22:03:55 +0300

    Создана модель User

---
+++ chapter_004/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,17 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+
+public final class User {
+
+    public String name;
+    public int children;
+    public Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+}
