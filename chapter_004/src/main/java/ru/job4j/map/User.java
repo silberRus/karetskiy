8a233b41174f4dc2c1d53dfbfb1c407b4c766797
Andrey Karetskiy <kareckiyan@gmail.com>
2018-05-16 22:34:10 +0300

    Переопределить только hashCode

--- chapter_004/src/main/java/ru/job4j/map/User.java
+++ chapter_004/src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,7 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.Objects;
 
 public final class User {
 
@@ -13,5 +14,11 @@ public final class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(name, children, birthday);
     }
 }
